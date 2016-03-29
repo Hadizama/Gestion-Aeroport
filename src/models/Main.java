acfb66fe4efbead1d2a9ecdd5862b26b2fed369d
raptack <Raphael@Raphael-vaio>
2016-03-29 02:05:46 +0200

    main

--- src/models/Main.java
+++ src/models/Main.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 
 import vue.Fenetre;
+import controllers.AgentController;
 import controllers.MainController;
 import controllers.VolController;
 
@@ -13,42 +14,43 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		MainController mc = new MainController(null);
+		MainController mc = new MainController(null, null);
+		AgentController ma = new AgentController(null, null);
+		VolController mv = new VolController(null, null);
+		
+		mc.importationFichiers();
+		mc.genererPlanning();
+		
+		// Annulation d'un vol
+		System.out.println("Nb vols : " + Vol.getLesVols().size());
+		System.out.println("Nb taches : " + TacheVol.getLesTachesVol().size());
+		System.out.println("Nombre de tache pour ce vol : " + Vol.getLesVols().get("IT1111").getLesTaches().size() );
+		mv.annulerVol("IT1111");
+		System.out.println("Nb vols : " + Vol.getLesVols().size());
+		System.out.println("Nb taches : " + TacheVol.getLesTachesVol().size());
+		
+		System.out.println("\n----------------------------------------------------------------\n");
+		
+		// Retard
+		System.out.println(Vol.getLesVols().get("CAL917") +"\n\nListe des tâches affectées à ce vol :");
+		Vol vol = Vol.getLesVols().get("CAL917");
+		ArrayList<Tache> taches = Tache.trier(vol.getLesTaches());
+		for (Tache tache : taches) {
+			if( ((TacheVol)tache).getNumeroVol().equals("CAL917") && !Tache.listeTachesNonAffectees().contains(tache) && tache.getAgent() != null)
+				System.out.println("\t" + ((TacheVol)tache).toString() + " (" + tache.getAgent().getMatricule() + ", "+ tache.getAgent().getNom() + ")\n");
+		}
+		
+		mv.retarderVol("CAL917", 9, 20);		
+		System.out.println("----------------------------------------");		
+		
+		vol = Vol.getLesVols().get("CAL917");
+		taches = Tache.trier(vol.getLesTaches());
+		for (Tache tache : taches) {
+			if( ((TacheVol)tache).getNumeroVol().equals("CAL917") && !Tache.listeTachesNonAffectees().contains(tache) && tache.getAgent() != null)
+				System.out.println("\t" + ((TacheVol)tache).toString() + " (" + tache.getAgent().getMatricule() + ", "+ tache.getAgent().getNom() + ")\n");
+		}
 		
 		
-		/*AgentTempsPlein a = new AgentTempsPlein("sfsdfsdfds", "José", "Bové", 2);
-
-	/*	Tache t2 = new TacheAccueil("tache2", new Horaire(10, 30), new Horaire(10, 40));
-		Tache t1 = new TacheAccueil("tache1", new Horaire(9, 10), new Horaire(10, 10));
-		a.affecterTache(t2);
-		a.affecterTache(t1);
-						
-//		a.affecterTache(TacheDebarquement.lesTachesDebarquement.get("IT4444"));
-		
-		a.afficherPlanningTache();
-*/
-		
-		/*for(int i=0;i<20;i++){
-			System.out.println(Agent.getHRepas());
-		}*/
-		
-//		mc.genererPlanning();
-//		ArrayList<Agent> ag = Agent.trier(Agent.getLesAgents());
-//		int i = 0;
-//		for(Agent a : ag){
-//			System.out.println(a);
-//			i+= a.getLesTaches().size();
-//		}
-//		System.out.println(i);
-//		System.out.println(Tache.listeTachesNonAffectees());
-//		System.out.println(Tache.listeTachesNonAffectees().size());
-		
-		//System.out.println(Tache.trier(TacheRepas.getLesTachesRepas()));
-		
-		//System.out.println(TacheVol.getLesTachesVol().size());
-		//System.out.println(Agent.trier(AgentMiTemps.getLesAgentsMiTemps()).toString());
-	//	System.out.println(TacheEmbarquement.getLesTachesEmbarquement().size());
-	//	System.out.println(TacheDebarquement.getLesTachesDebarquement().toString());
 	}
 
 }
